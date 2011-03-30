fd9868bdb31bfa3832c7eced7c2f6136bb8651a3
Lingfeng Yang <lyang@dn5221bc.sunet>
2011-03-30 15:59:59 -0700

    fg2image renders 10 samples per fg

--- python/fg2image.py
+++ python/fg2image.py
@@ -7,26 +7,27 @@ import os
 if __name__ == "__main__":
     args=pickle.load(sys.stdin)
 
-    nodes, result_fg = mkFG(args)
 
-    debug = mkDebugChannel('fg2image_debug')
+    for i in range(10):
+        nodes, result_fg = mkFG(args)
+        debug = mkDebugChannel('fg2image_debug')
 
-    print >>debug, 'begin fg2image ====================='
-    print >>debug, 'input', pformat(args)
-    print >>debug, 'factors', pformat(result_fg)
-    print >>debug, 'nodes', pformat(nodes)
-    print >>debug, 'nodepos', pformat(map(lambda n: n.tile_obj.pos, nodes))
+        print >>debug, 'begin fg2image ====================='
+        print >>debug, 'input', pformat(args)
+        print >>debug, 'factors', pformat(result_fg)
+        print >>debug, 'nodes', pformat(nodes)
+        print >>debug, 'nodepos', pformat(map(lambda n: n.tile_obj.pos, nodes))
 
-    nodes, score = sampleEltFG(nodes, result_fg, MH(10000))
+        nodes, score = sampleEltFG(nodes, result_fg, MH(10000))
+        drawFG(nodes, map(lambda f: f.variables, result_fg))
 
-    print >>debug, 'after sampling:'
-    print >>debug, 'score:', score
-    print >>debug, 'nodes', pformat(nodes)
-    print >>debug, 'nodepos', pformat(map(lambda n: n.tile_obj.pos, nodes))
-    print >>debug, 'end fg2image ====================='
+        print >>debug, 'after sampling:'
+        print >>debug, 'score:', score
+        print >>debug, 'nodes', pformat(nodes)
+        print >>debug, 'nodepos', pformat(map(lambda n: n.tile_obj.pos, nodes))
+        print >>debug, 'end fg2image ====================='
 
-    debug.close()
+        debug.close()
 
-    drawFG(nodes, map(lambda f: f.variables, result_fg))
 
     pickle.dump(args,sys.stdout)
