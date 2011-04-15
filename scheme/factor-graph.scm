17cda15e46cee2bba49882e127071b1e603ce603
ih <irvin.hwang@gmail.com>
2011-04-15 16:01:59 -0700

    rewrote make-initial-sexpr with completing the pipeline in mind

--- scheme/factor-graph.scm
+++ scheme/factor-graph.scm
@@ -1,23 +1,36 @@
 ;;functions related to factor graphs
 (library (factor-graph)
-         (export python-format->scheme-program ghost-node? python-data->scheme python-node->scheme python-ghost-node->scheme data?)
+         (export python-format->scheme-program ghost-node? python-data->scheme python-node->scheme python-ghost-node->scheme data? image->factor-graph)
          (import (except (rnrs) string-hash string-ci-hash)
                  (church readable-scheme)
                  (_srfi :1))
          (define GHOST-NODE 'GN)
          (define NODE 'N)
          (define DATA 'data)
+         ;;temporary definition, should be a python function
+         (define (image->factor-graph image)
+           '(N (data (label 1) (radius 10) (blobbiness 3.5) (Distance 5 .5) (Straightness 0 0.1))
+                 (N (data (label 2) (radius 5) (blobbiness 3.5) (Distance 3 .5) (Straightness 0 0.1))
+                    (N (data (label 3) (radius 2) (blobbiness 3.5) (Distance 2 .5) (Straightness 0 0.1))
+                       (N (data (label 4) (radius 5) (blobbiness 10) (Distance 5 .5) (Straightness 0 0.1)))
+                       (N (data (label 5) (radius 5) (blobbiness 10) (Distance 5 .5) (Straightness 0 0.1)))))))
 ;;;basically put 'node at the front of every sub-expr and wrap in a lambda
          (define (python-format->scheme-program py-format)
            (let* ([body (convert-py-format py-format)]) 
              (list 'lambda '() body)))
 
+         ;; (define (convert-py-format py-sexpr)
+         ;;   (cond [(null? py-sexpr) py-sexpr]
+         ;;         [(ghost-node? py-sexpr) (python-ghost-node->scheme py-sexpr)]
+         ;;         [(node? py-sexpr) (python-node->scheme py-sexpr)]
+         ;;         [(data? py-sexpr) (python-data->scheme py-sexpr)]
+         ;;         [else (error 'convert-py-format "unrecognized python expression for factor graph" py-sexpr)]))
+
+         ;;works if we know what the data attributes are ahead of time and can define functions for each of the attribute names
          (define (convert-py-format py-sexpr)
-           (cond [(null? py-sexpr) py-sexpr]
-                 [(ghost-node? py-sexpr) (python-ghost-node->scheme py-sexpr)]
-                 [(node? py-sexpr) (python-node->scheme py-sexpr)]
-                 [(data? py-sexpr) (python-data->scheme py-sexpr)]
-                 [else (error 'convert-py-format "unrecognized python expression for factor graph" py-sexpr)]))
+           py-sexpr)
+
+
 
          (define (ghost-node? py-sexpr)
            (tagged-list? py-sexpr GHOST-NODE))
