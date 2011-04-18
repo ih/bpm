2e2471cb4d1366fc4c73e15f062948e314aded1a
ih <irvin.hwang@gmail.com>
2011-04-17 18:04:39 -0700

    added test for remove-abstraction-variable and adjusted implmentation

--- scheme/dearguments.scm
+++ scheme/dearguments.scm
@@ -4,40 +4,42 @@
                  (program)
                  (_srfi :1)
                  (util))
-
-         ;;a transformation is performed for each variable of each abstraction 
-         (define (dearguments program . nofilter)
-           (let* ([abstractions-with-variables (filter has-arguments? (program->abstractions program))]
-                  [deargumented-programs (concatenate (map (curry abstraction-deargumentations program) abstractions-with-variables))]
-                  [program-size (size (program->sexpr program))]
-                  [valid-deargumented-programs
-                   (if (not (null? nofilter))
-                       deargumented-programs
-                       (filter (lambda (ip) (<= (size (program->sexpr ip))
-                                                (+ program-size 1)))
-                               deargumented-programs))])
-             valid-deargumented-programs))
+         ;;creates a program transformation that removes a variable from the abstraction and replaces it with the output of replacement-function
+         ;;replacement-function takes in the instances for a particular variable and returns an expression that the variable gets set to 
+         (define (make-dearguments-transformation replacement-function)
+           ;;a transformation is performed for each variable of each abstraction 
+           (lambda (program . nofilter)
+             (let* ([abstractions-with-variables (filter has-arguments? (program->abstractions program))]
+                    [deargumented-programs (concatenate (map (curry abstraction-deargumentations replacement-function program) abstractions-with-variables))]
+                    [program-size (size (program->sexpr program))]
+                    [valid-deargumented-programs
+                     (if (not (null? nofilter))
+                         deargumented-programs
+                         (filter (lambda (ip) (<= (size (program->sexpr ip))
+                                                  (+ program-size 1)))
+                                 deargumented-programs))])
+               valid-deargumented-programs)))
 
          (define (has-arguments? abstraction)
            (not (null? (abstraction->vars abstraction))))
 
          ;;return a program transformation is returned for each variable in abstraction
-         (define (abstraction-deargumentations program abstraction)
-           (map (curry deargument program abstraction) (abstraction->vars abstraction)))
+         (define (abstraction-deargumentations replacement-function program abstraction)
+           (map (curry deargument replacement-function program abstraction) (abstraction->vars abstraction)))
 
          ;;rewrite the abstraction to have the variable in the abstraction be a mixture of the values its taken on in the program
          ;;rewrite applications of the abstraction function in the program to not have the variable
-         (define (deargument program abstraction variable)
-           (let* ([new-abstraction (remove-abstraction-variable program abstraction variable)]
+         (define (deargument replacement-function program abstraction variable)
+           (let* ([new-abstraction (remove-abstraction-variable replacement-function program abstraction variable)]
                   [program-with-new-abstraction (program->replace-abstraction program new-abstraction)]
                   [new-program (remove-application-argument program-with-new-abstraction abstraction variable)])
              new-program))
 
          ;;creates a "mixture" distribution over instances of the variable being removed
-         (define (remove-abstraction-variable program abstraction variable)
-           (let* ([mixture-elements (find-variable-instances program abstraction variable)]
-                  [mixture-sexpr (make-mixture-sexpr mixture-elements)]
-                  [new-pattern `((lambda (,variable) ,(abstraction->pattern abstraction)) ,mixture-sexpr)]
+         (define (remove-abstraction-variable replacement-function program abstraction variable)
+           (let* ([variable-instances (find-variable-instances program abstraction variable)]
+                  [variable-definition (replacement-function variable-instances)]
+                  [new-pattern `((lambda (,variable) ,(abstraction->pattern abstraction)) variable-definition)]
                   [new-variables (delete variable (abstraction->vars abstraction))])
              (make-named-abstraction (abstraction->name abstraction) new-pattern new-variables)))
 
