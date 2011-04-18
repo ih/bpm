c3d908acb14a6451c7c0fc61241905479e14bb07
lyang <lyang@dnab434deb.stanford.edu>
2011-04-18 16:28:10 -0700

    adding test case and an initial version of im->fg

--- python/im2fg.py
+++ python/im2fg.py
@@ -3,49 +3,65 @@ import test
 
 from fg_interface import *
 
+from utils import *
+
 fsts = lambda xs: map(lambda (x, y): x, xs)
 
-class Node(object):
-    def __init__(self, name):
-        self.name = name
-        self.children = []
-    def __repr__(self):
-        return 'Node'
+
+Node = Term('Node', ['label', 'radius', 'blobbiness', "Distance", 'Straightness', 'children'])
+
+get_ints = lambda xs: map(int, xs)
+get_floats = lambda xs: map(float, xs)
+
+get_one_int = lambda xs: get_ints(xs)[0]
+get_one_float = lambda xs: get_floats(xs)[0]
+
+parses = {
+        'label' : get_one_int,
+        'radius': get_one_float,
+        'blobbiness': get_one_float,
+        'Distance': get_floats,
+        'Straightness': get_floats,
+        'children': get_ints
+        }
+
 
 def parse_img_line(line):
     split = filter(lambda s: s != '', line.strip().split(' '))
-    return split
+
+    split2 = map(lambda s: parses.has_key(s), split)
+
+    true_index = filter(lambda i: split2[i] == True, range(len(split2)))
+
+    parse_ranges = map(lambda (i, j): (i + 1, j), zip(true_index, true_index[1:] + [len(split2)]))
+
+    assoc_name_range = map(lambda (ti, (j, k)): (split[ti], split[j : k]), zip(true_index, parse_ranges))
+
+    parsed_data = dict(map(lambda (name, data): (name, parses[name](data)), assoc_name_range))
+
+    node = Node(
+            parsed_data['label'],
+            parsed_data['radius'],
+            parsed_data['blobbiness'],
+            parsed_data['Distance'],
+            parsed_data['Straightness'],
+            parsed_data.get('children', []))
+
+    return node
 
 if __name__ == "__main__":
-    #args=pickle.load(sys.stdin)
+    args=pickle.load(sys.stdin)
 
-    filename = sys.argv[1]
+    filename = args
 
-    print map(parse_img_line, open(filename).readlines())
-    sys.exit()
+    nodes = map(parse_img_line, open(filename).readlines())
 
-    raw1 = [filter(lambda s: s != '', s.strip('\n').split(' ')) for s in open(filename).readlines() if s != '']
-
-    print raw1
-
-    raw2 = map(lambda xs: (Node(xs[0]), map(int, xs[1:])), raw1)
-
-    print raw2
-
-    for (n, cs) in raw2:
-        n.children = map(lambda i: raw2[i - 1][0], cs)
-
-    raw3 = fsts(raw2) 
-
-    print raw3
-
-    mklist = lambda *a: a
+    mklist = lambda *a: list(a)
 
     def mk_sexpr(node):
-        return mklist(node, *map(mk_sexpr, node.children))
+        return mklist('N', ['data', ['label', node.label], ['radius', node.radius], ['blobbiness', node.blobbiness], ['Distance', node.Distance], ['Straightness', node.Straightness]], *map(mk_sexpr, map(lambda i: nodes[i], node.children)))
 
-    print mk_sexpr(raw3[0])
+    output = mk_sexpr(nodes[0])
 
-
-    pickle.dump(filename,sys.stdout)
+    pickle.dump(output,sys.stdout)
 
