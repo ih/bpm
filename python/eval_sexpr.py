767a4e9e8f0f96e75e9f28c03f7c56fa41afce27
lyang <lyang@dnab434deb.stanford.edu>
2011-04-18 17:51:42 -0700

    handle non-unique labels

--- python/eval_sexpr.py
+++ python/eval_sexpr.py
@@ -4,7 +4,7 @@ from fglib import *
 class Ghost(Elt):
     pass
 
-test = ['GN2', ['GN1',
+test = ['GN1', ['GN1',
                      ['N3', ['data', ['radius', 1.2], ['blobbiness', -0.2],
 ['Distance', 2, 0.1], ['Straightness', 0, 0.1]],
                          ['N2', ['data', ['radius', 0.8], ['blobbiness', -0.1],
@@ -21,19 +21,20 @@ test_simple = ['GN1',
                              
                          
 cmd_dict = {
-        'pos': lambda nodeName, val, node_dict, node_stack: setEltAttr(nodeName, "pos", tuple(val), node_dict),
-        'radius': lambda nodeName, val, node_dict, node_stack: setEltAttr(nodeName, "radius", val, node_dict),
-        'blobbiness': lambda nodeName, val, node_dict, node_stack: setEltAttr(nodeName, "blobbiness", val, node_dict),
-        'Distance': lambda nodeName, val, node_dict, node_stack: makeFactor(
-            [node_dict[nodeName], node_stack[-1]],
+        'label': lambda latest, val, node_dict, node_stack: setEltAttr(latest, "label", val, node_dict),
+        'pos': lambda latest, val, node_dict, node_stack: setEltAttr(latest, "pos", tuple(val), node_dict),
+        'radius': lambda latest, val, node_dict, node_stack: setEltAttr(latest, "radius", val, node_dict),
+        'blobbiness': lambda latest, val, node_dict, node_stack: setEltAttr(latest, "blobbiness", val, node_dict),
+        'Distance': lambda latest, val, node_dict, node_stack: makeFactor(
+            [node_dict[latest], node_stack[-1]],
             lambda x, y: make_distgauss(*val)(x.pos, y.pos)),
-        'Straightness': lambda nodeName, val, node_dict, node_stack: makeFactor([node_dict[nodeName], node_stack[-1], node_stack[-2]], 
+        'Straightness': lambda latest, val, node_dict, node_stack: makeFactor([node_dict[latest], node_stack[-1], node_stack[-2]], 
             lambda x, y, z: make_straightgauss(*val)(x.pos, y.pos, z.pos))
         }
-mkParent = lambda nodeName, val, node_dict, node_stack: setEltAttr(nodeName, "parent", node_stack[-1], node_dict)
+mkParent = lambda latest, val, node_dict, node_stack: setEltAttr(latest, "parent", node_stack[-1], node_dict)
 
-def setEltAttr(nodeName, name, val, node_dict):
-    node_dict[nodeName].makeNewField(name, val)
+def setEltAttr(latest, name, val, node_dict):
+    node_dict[latest].makeNewField(name, val)
 
 def makeFactor(scope, fx): 
     factor = Factor(*scope)
@@ -49,20 +50,24 @@ isGN = lambda n: n.tile_obj.name.startswith('GN')
 
 def evalFactorTree(ftree, node_stack, result_fg, node_dict):
     nodeName = ftree[0]
-    if not node_dict.has_key(nodeName):
-        node_dict[nodeName] = Elt(nodeName) if not nodeName.startswith('GN') else Ghost(nodeName)
-        node_dict[nodeName].tile_obj = Tile({})
+    latest = Elt(nodeName) if not nodeName.startswith('GN') else Ghost(nodeName)
+    node_dict[latest] = latest
+    node_dict[latest].tile_obj = Tile({})
 
-    new_node_stack = node_stack + [node_dict[nodeName]]
+    #if not node_dict.has_key(nodeName):
+    #    node_dict[nodeName] = Elt(nodeName) if not nodeName.startswith('GN') else Ghost(nodeName)
+    #    node_dict[nodeName].tile_obj = Tile({})
+
+    new_node_stack = node_stack + [node_dict[latest]]
 
     data, children = partition(lambda elem: elem[0] == "data", ftree[1:])
 
     if len(data) > 0:
         data = data[0]
         data = map(lambda n_v: (n_v[0], tuple(n_v[1:]) if len(n_v) > 2 else n_v[1]),  data[1:])
-        new_factors = map(lambda (name, val): cmd_dict[name](nodeName, val, node_dict, node_stack), data)
+        new_factors = map(lambda (name, val): cmd_dict[name](latest, val, node_dict, node_stack), data)
         if len(node_stack) >= 2:
-            map(lambda (name, val): mkParent(nodeName, val, node_dict, node_stack), data)
+            map(lambda (name, val): mkParent(latest, val, node_dict, node_stack), data)
 
         new_factors = filter(lambda x: type(x) == Factor, new_factors)
         result_fg += new_factors
@@ -80,7 +85,10 @@ def finalizeNodes(node_dict):
 
     return map(lambda (name, elt): elt.makeNewField('pos', (0,0)), node_dict.items())
 
-#result_factor_graph = [] 
-#nodes = {}
-#evalFactorTree(test, [], result_factor_graph, nodes)
+result_factor_graph = [] 
+nodes = {}
+evalFactorTree(test, [], result_factor_graph, nodes)
+
+print finalizeNodes(nodes)
+print result_factor_graph
 
