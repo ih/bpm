52c64950cd8616013ba9a7d8f540c7c36d4845c3
ih <irvin.hwang@gmail.com>
2011-05-05 16:01:35 -0700

    replace-color tests passing

--- scheme/tests/tree-tests.scm
+++ scheme/tests/tree-tests.scm
@@ -1,11 +1,15 @@
 (import (tree)
         (srfi :78)
         (_srfi :1)
+        (program)
         (church readable-scheme))
 
 ;;;node-data->expression
-(check (node-data->expression '((.1) (1))) => '(data (color .1) (size 1)))
+(check (node-data->expression '(data (.1) (1))) => '(data (color .1) (size 1)))
+;;;replace-color
+(check (replace-color (make-program (list (make-named-abstraction 'F1 '(V1) '(N (data (color V1) (size 2))))) '(N (data (color 3) (size 1)) (F1 10) (F1 10)))) => (make-program (make-named-abstraction '(V1) 'F1 '(N (data (list V1 15) (size 2)))) '(N (data (list 3 15) (size 1)) (F1 10) (F1 10))))
 
+(check (eval (replace-color (make-program (list (make-named-abstraction 'F1 '(N (data (color V1) (size 2))) '(V1))) '(N (data (color 3) (size 1)) (F1 10) (F1 10)))) (interaction-environment)) => '((data (3 15) (1)) ((data (10 15) (2))) ((data (10 15) (2)))))
 ;;;ghost-node? tests
 (check-report)
 (exit)
--- scheme/tree.scm
+++ scheme/tree.scm
@@ -1,7 +1,9 @@
 ;;functions related to factor graphs
 (library (tree)
-         (export tree->expression node-data->expression)
+         (export tree->expression node-data->expression replace-color)
          (import (except (rnrs) string-hash string-ci-hash)
+                 (program)
+                 (util)
                  (church readable-scheme)
                  (_srfi :1))
          (define GHOST-NODE 'GN)
@@ -15,4 +17,17 @@
                  (pair 'N (pair (node-data->expression (first tree)) (map tree->expression (rest tree))))))
 
          (define (node-data->expression lst)
-           `(data ,(pair 'color (second lst)) ,(pair 'size (third lst)))))
+           `(data ,(pair 'color (second lst)) ,(pair 'size (third lst))))
+
+         ;;used in scoring (computing the likelihood for a program that generates trees)
+         ;;replace-color::program->program
+         (define (replace-color program)
+           (define (color? sexpr)
+             (tagged-list? sexpr 'color))
+           (define (return-parameters sexpr)
+             `(list ,(second sexpr) 15))
+           (define (replace-in-abstraction abstraction)
+             (make-named-abstraction (abstraction->name abstraction) (sexp-search color? return-parameters (abstraction->pattern abstraction)) (abstraction->vars abstraction)))
+           (let* ([converted-abstractions (map replace-in-abstraction (program->abstractions program))]
+                  [converted-body (sexp-search color? return-parameters (program->body program))])
+             (make-program converted-abstractions converted-body))))
