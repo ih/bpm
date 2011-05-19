edc09e0e69b66750ba764b9ea491d0c39129c126
ih <irvin.hwang@gmail.com>
2011-05-19 15:03:53 -0700

    added program transformation to likelihood calculation so that we have a conditional (uniform-choice) that has delayed evaluation of its arguments

--- scheme/tests/tree-tests.scm
+++ scheme/tests/tree-tests.scm
@@ -5,11 +5,12 @@
         (church readable-scheme))
 
 ;;;node-data->expression
-(check (node-data->expression '(data (.1) (1))) => '(data (color .1) (size 1)))
+(check (node-data->expression '(data (.1) (1))) => '(data (color (gaussian .1 25)) (size 1)))
 ;;;replace-color
-(check (replace-gaussian (make-program (list (make-named-abstraction 'F1 '(V1) '(N (data (color V1) (size 2))))) '(N (data (color (gaussian 3 15)) (size 1)) (F1 10) (F1 10)))) => (make-program (make-named-abstraction 'F1 '(V1) '(N (data (color V1) (size 2)))) '(N (data (list 'gaussian-parameters 3 15) (size 1)) (F1 10) (F1 10))))
+(check (desugar (make-program (list (make-named-abstraction 'F1 '(N (data (color V1) (size 2))) '(V1)))
+                              '(uniform-choice (N (data (color (gaussian 3 15)) (size 1)) (F1 10) (F1 10))))) => (make-program (list (make-named-abstraction 'F1  '(N (data (color V1) (size 2))) '(V1))) '((uniform-draw (list (lambda () (N (data (color (list 'gaussian-parameters 3 15)) (size 1)) (F1 10) (F1 10))))))))
 
-;(check (eval (replace-gaussian (make-program (list (make-named-abstraction 'F1 '(N (data (color V1) (size 2))) '(V1))) '(N (data (color 3) (size 1)) (F1 10) (F1 10)))) (interaction-environment)) => '((data (3 15) (1)) ((data (10 15) (2))) ((data (10 15) (2)))))
+;(check (eval (desugar (make-program (list (make-named-abstraction 'F1 '(N (data (color V1) (size 2))) '(V1))) '(N (data (color 3) (size 1)) (F1 10) (F1 10)))) (interaction-environment)) => '((data (3 15) (1)) ((data (10 15) (2))) ((data (10 15) (2)))))
 ;;;ghost-node? tests
 (check-report)
 (exit)
--- scheme/tree.scm
+++ scheme/tree.scm
@@ -1,6 +1,6 @@
 ;;functions related to factor graphs
 (library (tree)
-         (export tree->expression node-data->expression replace-gaussian gaussian->mean gaussian->variance)
+         (export tree->expression node-data->expression desugar gaussian->mean gaussian->variance)
          (import (except (rnrs) string-hash string-ci-hash)
                  (program)
                  (util)
@@ -21,15 +21,25 @@
 
          ;;used in scoring (computing the likelihood for a program that generates trees)
          ;;replace-color::program->program
-         (define (replace-gaussian program)
+         (define (desugar program)
            (define (gaussian? sexpr)
              (tagged-list? sexpr 'gaussian))
+           (define (uniform-choice? sexpr)
+             (tagged-list? sexpr 'uniform-choice))
            (define (return-parameters sexpr)
              `(list 'gaussian-parameters ,(second sexpr) ,(third sexpr)))
-           (define (replace-in-abstraction abstraction)
-             (make-named-abstraction (abstraction->name abstraction) (sexp-search gaussian? return-parameters (abstraction->pattern abstraction)) (abstraction->vars abstraction)))
-           (let* ([converted-abstractions (map replace-in-abstraction (program->abstractions program))]
-                  [converted-body (sexp-search gaussian? return-parameters (program->body program))])
+           (define (uniform-draw-conversion sexpr)
+             `((uniform-draw (list ,@(map thunkify (rest sexpr))))))
+           (define tests+replacements (zip (list gaussian? uniform-choice?) (list return-parameters uniform-draw-conversion)))
+           (define (apply-transforms sexpr)
+             (fold (lambda (test+replacement expr)
+                     (sexp-search (first test+replacement) (second test+replacement) expr))
+                   sexpr
+                   tests+replacements))
+           (define (desugar-abstraction abstraction)
+             (make-named-abstraction (abstraction->name abstraction) (apply-transforms (abstraction->pattern abstraction)) (abstraction->vars abstraction)))
+           (let* ([converted-abstractions (map  desugar-abstraction (program->abstractions program))]
+                  [converted-body (apply-transforms (program->body program))])
              (make-program converted-abstractions converted-body)))
 
          (define gaussian->mean second)
