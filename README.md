4c7a07ad1c977e341e1740c46e03eeb9661e14f7
ih <irvin.hwang@gmail.com>
2011-06-22 20:09:34 -0700

    separated replace gaussians transformation out of desugar otherwise error drawing trees by running a desugared program

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-You need to have ikarus, bher, and [scheme-tools](https://github.com/stuhlmueller/scheme-tools) set up on your system. <code>[fg-dir]</code> is the absolute path for the factor-graphics directory e.g. <code>/home/ih/factor-graphics/</code> (it is important to have the trailing '/'). <code>[bher dir]</code> is the absolute path for the bher directory, e.g., <code>/home/ih/bher-read-only/</code>. 
+You need to have ikarus, bher, and [scheme-tools](https://github.com/stuhlmueller/scheme-tools) set up on your system. <code>[bpm-dir]</code> is the absolute path for the bayesian program merging directory e.g. <code>/home/ih/bpm/</code> (it is important to have the trailing '/'). <code>[bher dir]</code> is the absolute path for the bher directory, e.g., <code>/home/ih/bher-read-only/</code>. 
 
 # Installation
 
 1. Apply modifications to Bher:
 
-    1. Change into the factor-graphics directory: <code>cd [fg-dir]</code>
+    1. Change into the factor-graphics directory: <code>cd [bpm-dir]</code>
 
-    2. Run <code>python configure.py "[bher-dir]" "[fg-dir]"</code>
+    2. Run <code>python configure.py "[bher-dir]" "[bpm-dir]"</code>
 
 
-2. Adjust path variables: Add <code>PATH=$PATH:[bher dir]:.</code> and <code>IKARUS_LIBRARY_PATH=[fg-dir]/scheme:[scheme-tools dir]:[bher dir]:.</code> to your .bashrc file.
+2. Adjust path variables: Add <code>PATH=$PATH:[bher dir]:.</code> and <code>IKARUS_LIBRARY_PATH=[bpm-dir]/scheme:[scheme-tools dir]:[bher dir]:.</code> to your .bashrc file.
 
 # Testing
 
 To see if everything works you can do the following
 
-    cd [fg-dir]/church/tests
-    bher factor-graph-tests.church
+    cd [bpm-dir]/church/tests
     bher beam-learning-tests.church
 
 Due to path issues, you currently have to run bher from the directory containing the church file being run if it uses eval.
--- scheme/tree.scm
+++ scheme/tree.scm
@@ -1,6 +1,6 @@
 ;;functions related to factor graphs
 (library (tree)
-         (export tree->expression node-data->expression desugar gaussian->mean gaussian->variance)
+         (export tree->expression node-data->expression desugar gaussian->mean gaussian->variance replace-gaussian)
          (import (except (rnrs) string-hash string-ci-hash)
                  (program)
                  (util)
@@ -30,7 +30,7 @@
              `(list 'gaussian-parameters ,(second sexpr) ,(third sexpr)))
            (define (uniform-draw-conversion sexpr)
              `((uniform-draw (list ,@(map thunkify (rest sexpr))))))
-           (define tests+replacements (zip (list gaussian? uniform-choice?) (list return-parameters uniform-draw-conversion)))
+           (define tests+replacements (zip (list uniform-choice?) (list uniform-draw-conversion)))
            (define (apply-transforms sexpr)
              (fold (lambda (test+replacement expr)
                      (sexp-search (first test+replacement) (second test+replacement) expr))
@@ -40,6 +40,17 @@
              (make-named-abstraction (abstraction->name abstraction) (apply-transforms (abstraction->pattern abstraction)) (abstraction->vars abstraction)))
            (let* ([converted-abstractions (map  desugar-abstraction (program->abstractions program))]
                   [converted-body (apply-transforms (program->body program))])
+             (make-program converted-abstractions converted-body)))
+
+         (define (replace-gaussian program)
+           (define (gaussian? sexpr)
+             (tagged-list? sexpr 'gaussian))
+           (define (return-parameters sexpr)
+             `(list 'gaussian-parameters ,(second sexpr) ,(third sexpr)))
+           (define (replace-in-abstraction abstraction)
+             (make-named-abstraction (abstraction->name abstraction) (sexp-search gaussian? return-parameters (abstraction->pattern abstraction)) (abstraction->vars abstraction)))
+           (let* ([converted-abstractions (map replace-in-abstraction (program->abstractions program))]
+                  [converted-body (sexp-search gaussian? return-parameters (program->body program))])
              (make-program converted-abstractions converted-body)))
 
          (define gaussian->mean second)
